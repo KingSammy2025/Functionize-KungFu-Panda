d5a0de4624dec5c0ceb236419099182cfabb4ad5
KingSammy2025 <o.ibikunle-philips@share.epsb.ca>
2025-02-26 14:18:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,62 +10,43 @@ function searchClicked() {
   name = name.toLowerCase();
 
   // Test Input Variable and update the page
-  if (name === "Po" || name === "dragon warrior") {
+  if (name === "po" || name === "dragon warrior") {
     // Update page to Po
-    characters("Po", "images", "Buddy, I am the Dragon Warrior");
-
-    ("Buddy, I am the Dragon Warrior.");
+    characters("Po", "Buddy, I am the Dragon Warrior");
   } else if (name === "tigress" || name === "master tigress") {
     // Update page to Tigress
-    characters(
-      "Tigress",
-      `${images}-images/tigress.png`,
-      "That was pretty hardcore!"
-    );
+    characters("Tigress", "That was pretty hardcore!");
   } else if (name === "mantis" || name === "master mantis") {
     // Update page to Mantis
-    document.getElementById("character-name").innerHTML = "Master Mantis";
-    document.getElementById("main-img").src = "images/mantis.png";
-    document.getElementById("quote").innerHTML = "Fear the bug!";
+    characters("Mantis", "Fear the bug!");
   } else if (name === "monkey" || name === "master monkey") {
     // Update page to Monkey
-    document.getElementById("character-name").innerHTML = "Master Monkey";
-    document.getElementById("main-img").src = "images/monkey.png";
-    document.getElementById("quote").innerHTML = "We should hang out.";
+    characters("Monkey", "We should hang out.");
   } else if (name === "crane" || name === "master crane") {
     // Update page to Crane
-    document.getElementById("character-name").innerHTML = "Master Crane";
-    document.getElementById("main-img").src = "images/crane.png";
-    document.getElementById("quote").innerHTML =
-      "You can chain my body, but you will never chain my warrior spirit!";
+    characters(
+      "Crane",
+      "You can chain my body, but you will never chain my warrior spirit!"
+    );
   } else if (name === "viper" || name === "master viper") {
     // Update page to Viper
-    document.getElementById("character-name").innerHTML = "Master Viper";
-    document.getElementById("main-img").src = "images/viper.png";
-    document.getElementById("quote").innerHTML =
-      "I don't need to bite to fight!";
+    characters("Viper", "I don't need to bite to fight!");
   } else if (name === "shifu" || name === "master shifu") {
     // Update page to Master Shifu
-    document.getElementById("character-name").innerHTML = "Master Shifu";
-    document.getElementById("main-img").src = "images/shifu.png";
-    document.getElementById("quote").innerHTML = "There is now a Level Zero.";
+    characters("Shifu", "There is now a Level Zero.");
   } else if (name === "ping" || name === "mr. ping") {
     // Update page to Mr. Ping
-    document.getElementById("character-name").innerHTML = "Mr. Ping";
-    document.getElementById("main-img").src = "images/mr-ping.png";
-    document.getElementById("quote").innerHTML =
-      "We are noodle folk, broth runs through our veins!";
+    characters("Ping", "We are noodle folk, broth runs through our veins!");
   } else {
     // Update page to Question Mark
-    document.getElementById("character-name").innerHTML = "?????";
-    document.getElementById("main-img").src = "images/question-mark.png";
-    document.getElementById("quote").innerHTML = "Character Not Found";
+    characters("question-mark", "Character Not Found");
   }
 }
 
 // Helper functions
-function characters(name, images, quote) {
+function characters(name, quote) {
   document.getElementById("character-name").innerHTML = name;
-  document.getElementById("main-img").src = images;
+  document.getElementById("main-img").src = `images/${name}.png`;
   document.getElementById("quote").innerHTML = `${quote}`;
 }
+
